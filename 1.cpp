79bda47acab774813564b6bc789785fd7ace6dcb
BestChennn <76035455+BestChennn@users.noreply.github.com>
2020-12-25 15:59:25 +0700

    Fix output=12.5

---
+++ 1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double a  =  20, b = 30;
+    double c = 4, outpot;
+
+    outpot = a - (b/c);
+    cout << "output: "<< outpot << endl ;
+    return 0;
+}
