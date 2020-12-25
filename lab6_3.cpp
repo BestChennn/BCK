fe831b9090be6dcf8bf6ed9ba9a40faee487a2c9
BestChennn <bestchenbestchen@gmail.com>
2020-12-25 17:52:10 +0700

    fix6.3

--- lab6_3.cpp
+++ lab6_3.cpp
@@ -1,19 +1,14 @@
 #include<iostream>
 using namespace std;
 
-char before(char x){
-	//Write your function definition here
-}
-
-int main(){
-	//Test Case
-	cout << before('A') << "\n";
-	cout << before('B') << "\n";
-	cout << before('P') << "\n";
-	cout << before('T') << "\n";
-	cout << before('Z') << "\n";
-	cout << before('a') << "\n";
-	cout << before('0') << "\n";
-	cout << before('c') << "\n";
-	return 0;
+char before(char X){
+	if(X == 65){
+        return X = 'Z';
+    }
+    if(X >= 65 && X <=90){
+        X = X-1;
+    }else{
+        return '0';
+    }
+    return X;
 }
